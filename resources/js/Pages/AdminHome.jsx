b33f6aacf52dff383b18d48adb8e61446e299774
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-10-01 09:53:05 +0200

    page principal admin

--- resources/js/Pages/AdminHome.jsx
+++ resources/js/Pages/AdminHome.jsx
@@ -1,10 +1,21 @@
 import NavAdmin from '@/Components/NavAdmin'
 import React from 'react'
-
-export default function AdminHome() {
+import '../../css/admin.css'
+import Footer from '@/Components/Footer'
+export default function AdminHome({bannerImage}) {
   return (
     <div>
         <NavAdmin/>
+        <div className="carouDetailsnav">
+        <div className="div1details " style={{ marginLeft: '15%' }}>
+          <h2 className="detailsH1">Admin Dashboard</h2>
+          <p className="detailsP">Aranoz - Shop System</p>
+        </div>
+        <div className="div2details">
+          <img className="detailsCarouImg" src={bannerImage} alt="" />
+        </div>
+      </div>
+      <Footer/>
     </div>
   )
 }
